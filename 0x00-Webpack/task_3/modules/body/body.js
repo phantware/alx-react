cb64f4e422b1fb7e6698bc4ed30da3ef4adcfcfb
phantware <phantmoney2011@gmail.com>
2023-04-18 21:31:29 +0100

    body file modified

--- 0x00-Webpack/task_3/modules/body/body.js
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -1,17 +1,17 @@
-import "./body.css";
-const $ = require("jquery");
-const _ = require("lodash");
+import './body.css'
+const $ = require('jquery')
+const _ = require('lodash')
 
-$("body").append("<button>Click here to get started</button>");
-$("body").append('<p id="count"><p>');
+$('body').append('<button>Click here to get started</button>')
+$('body').append('<p id="count"><p>')
 
 const updateCounter = () => {
-  let times = $("#count").html() || 0;
-  $("button").on("click", () => {
-    times++;
-    $("#count").html(`${times} clicks on the button`);
-  });
-};
+  let times = $('#count').html() || 0
+  $('button').on('click', () => {
+    times++
+    $('#count').html(`${times} clicks on the button`)
+  })
+}
 
-_.debounce(updateCounter, 500);
-updateCounter();
+_.debounce(updateCounter, 500)
+updateCounter()
