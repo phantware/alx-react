d7edbcce768c3bbb7dab95fb2619f849ce198181
phantware <phantmoney2011@gmail.com>
2023-04-18 21:17:40 +0100

    dashboard modified

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,17 @@
-import $ from 'jquery';
-import _ from 'lodash';
+import $ from 'jquery'
+import _ from 'lodash'
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$('body').append('<p>Phantware Dashboard</p>')
+$('body').append('<p>Dashboard data for the students</p>')
+$('body').append('<button>Click here to get started</button>')
+$('body').append('<p id="count"></p>')
+$('body').append('<p>Copyright - Phantware School</p>')
 
-let conunt = 0;
+let conunt = 0
 
 function updateCounter() {
-  count++;
-  $("#count").html(`${count} clicks on the button`);
-};
+  count++
+  $('#count').html(`${count} clicks on the button`)
+}
 
-$('button').on('click', _.debounce(updateCounter, 500));
+$('button').on('click', _.debounce(updateCounter, 500))
