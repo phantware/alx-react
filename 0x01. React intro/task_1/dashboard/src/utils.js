c465cd172807f873d61a8881a927bad39059e3f0
phantware <phantmoney2011@gmail.com>
2023-04-20 23:36:48 +0100

    utils.js file modified

--- 0x01. React intro/task_1/dashboard/src/utils.js
+++ 0x01. React intro/task_1/dashboard/src/utils.js
@@ -1,3 +1,4 @@
-export const getFullYear = () => new Date().getFullYear();
+export const getFullYear = () => new Date().getFullYear()
 
-export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
+export const getFooterCopy = (isIndex) =>
+  isIndex ? 'Phantware School' : 'Phantware School main dashboard'
