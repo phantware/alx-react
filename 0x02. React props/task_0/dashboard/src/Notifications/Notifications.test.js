31edfe916cc5737999462f72f901783fddeed46c
phantware <phantmoney2011@gmail.com>
2023-04-25 21:42:47 +0100

    Notification.js.test file modified

--- 0x02. React props/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x02. React props/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -1,29 +1,31 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Notifications from "./Notifications";
+import React from 'react'
+import { shallow } from 'enzyme'
+import Notifications from './Notifications'
 
-describe("Notification component tests", () => {
-  it("renders Notification component without crashing", () => {
-    const notification = shallow(<Notifications />);
+describe('Notification component tests', () => {
+  it('renders Notification component without crashing', () => {
+    const notification = shallow(<Notifications />)
 
-    expect(notification).toBeDefined();
-  });
+    expect(notification).toBeDefined()
+  })
 
-  it("renders ul", () => {
-    const notification = shallow(<Notifications />);
+  it('renders ul', () => {
+    const notification = shallow(<Notifications />)
 
-    expect(notification.find("ul")).toBeDefined();
-  });
+    expect(notification.find('ul')).toBeDefined()
+  })
 
-  it("renders three list items", () => {
-    const notification = shallow(<Notifications />);
+  it('renders three list items', () => {
+    const notification = shallow(<Notifications />)
 
-    expect(notification.find("li")).toHaveLength(3);
-  });
+    expect(notification.find('li')).toHaveLength(3)
+  })
 
-  it("renders correct text", () => {
-    const notification = shallow(<Notifications />);
+  it('renders correct text', () => {
+    const notification = shallow(<Notifications />)
 
-    expect(notification.find("p").text()).toBe("Here is the list of notifications");
-  });
-});
+    expect(notification.find('p').text()).toBe(
+      'Here is the list of notifications'
+    )
+  })
+})
