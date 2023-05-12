fd60488f8b13e78b1e66ccaab19cd765614b7344
phantware <phantmoney2011@gmail.com>
2023-05-12 21:21:27 +0100

    CourseLists.js file modified

--- 0x02. React props/task_4/dashboard/src/CourseList/CourseList.js
+++ 0x02. React props/task_4/dashboard/src/CourseList/CourseList.js
@@ -1,21 +1,41 @@
-import React from "react";
-import "./CourseList.css";
-import CourseListRow from "./CourseListRow";
+import React from 'react'
+import './CourseList.css'
+import CourseListRow from './CourseListRow'
 
 function CourseList() {
   return (
-    <table id="CourseList">
+    <table id='CourseList'>
       <thead>
-        <CourseListRow textFirstCell="Available courses" textSecondCell={null} isHeader={true} />
-        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+        <CourseListRow
+          textFirstCell='Available courses'
+          textSecondCell={null}
+          isHeader={true}
+        />
+        <CourseListRow
+          textFirstCell='Course name'
+          textSecondCell='Credit'
+          isHeader={true}
+        />
       </thead>
       <tbody>
-        <CourseListRow textFirstCell="ES6" textSecondCell="60" isHeader={false} />
-        <CourseListRow textFirstCell="Webpack" textSecondCell="20" isHeader={false} />
-        <CourseListRow textFirstCell="React" textSecondCell="40" isHeader={false} />
+        <CourseListRow
+          textFirstCell='ES6'
+          textSecondCell='60'
+          isHeader={false}
+        />
+        <CourseListRow
+          textFirstCell='Webpack'
+          textSecondCell='20'
+          isHeader={false}
+        />
+        <CourseListRow
+          textFirstCell='React'
+          textSecondCell='40'
+          isHeader={false}
+        />
       </tbody>
     </table>
-  );
+  )
 }
 
-export default CourseList;
+export default CourseList
