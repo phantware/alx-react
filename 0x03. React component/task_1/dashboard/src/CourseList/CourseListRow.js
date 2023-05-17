1bdf52084877a6933468903369e7b230f71db135
phantware <phantmoney2011@gmail.com>
2023-05-17 16:39:08 +0100

    CourseListRow.js file modified

--- 0x03. React component/task_1/dashboard/src/CourseList/CourseListRow.js
+++ 0x03. React component/task_1/dashboard/src/CourseList/CourseListRow.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
   return (
@@ -20,18 +20,18 @@ function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
         </>
       )}
     </tr>
-  );
+  )
 }
 
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
   textFirstCell: PropTypes.string.isRequired,
   textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-};
+}
 
 CourseListRow.defaultProps = {
   isHeader: false,
   textSecondCell: null,
-};
+}
 
-export default CourseListRow;
+export default CourseListRow
