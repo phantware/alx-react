cc9fa3bc101cd7216d9500c9cc08b184d0629f7d
phantware <phantmoney2011@gmail.com>
2023-06-01 12:49:43 +0100

    NotificationItem.js file modified

--- 0x04. React inline styling/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x04. React inline styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,31 +1,44 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { StyleSheet, css } from "aphrodite";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, css } from 'aphrodite'
 
 class NotificationItem extends React.PureComponent {
   render() {
-    const { type, value, html, markAsRead, id } = this.props;
+    const { type, value, html, markAsRead, id } = this.props
     return (
       <>
         {type && value ? (
-          <li className={type === "default" ? css(styles.default) : css(styles.urgent)} onClick={() => markAsRead(id)} data-notification-type={type}>
+          <li
+            className={
+              type === 'default' ? css(styles.default) : css(styles.urgent)
+            }
+            onClick={() => markAsRead(id)}
+            data-notification-type={type}
+          >
             {value}
           </li>
         ) : null}
-        {html ? <li onClick={() => markAsRead(id)} data-urgent className={css(styles.urgent)} dangerouslySetInnerHTML={{ __html: html }}></li> : null}
+        {html ? (
+          <li
+            onClick={() => markAsRead(id)}
+            data-urgent
+            className={css(styles.urgent)}
+            dangerouslySetInnerHTML={{ __html: html }}
+          ></li>
+        ) : null}
       </>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   default: {
-    color: "blue",
+    color: 'blue',
   },
   urgent: {
-    color: "red",
+    color: 'red',
   },
-});
+})
 
 NotificationItem.propTypes = {
   type: PropTypes.string.isRequired,
@@ -35,14 +48,14 @@ NotificationItem.propTypes = {
   }),
   markAsRead: PropTypes.func,
   id: PropTypes.number,
-};
+}
 
 NotificationItem.defaultProps = {
-  type: "default",
+  type: 'default',
   markAsRead: () => {
-    console.log("empty func");
+    console.log('empty func')
   },
   id: 0,
-};
+}
 
-export default NotificationItem;
+export default NotificationItem
