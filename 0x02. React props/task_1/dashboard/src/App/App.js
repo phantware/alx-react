b76c5bb7c960bdd16b6b386e599861e699f0923e
phantware <phantmoney2011@gmail.com>
2023-06-16 23:20:52 +0100

    App.js file modified

--- 0x02. React props/task_1/dashboard/src/App/App.js
+++ 0x02. React props/task_1/dashboard/src/App/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Notifications from "../Notifications/Notifications";
-import Header from "../Header/Header";
-import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
-import "./App.css";
+import React from 'react'
+import Notifications from '../Notifications/Notifications'
+import Header from '../Header/Header'
+import Login from '../Login/Login'
+import Footer from '../Footer/Footer'
+import './App.css'
 
 function App() {
   return (
     <React.Fragment>
       <Notifications />
-      <div className="App">
+      <div className='App'>
         <Header />
         <Login />
         <Footer />
       </div>
     </React.Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
