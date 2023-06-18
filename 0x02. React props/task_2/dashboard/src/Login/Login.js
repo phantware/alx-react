b48d00f63fcf1b10ca176db33fb9494a9fca8ef6
phantware <phantmoney2011@gmail.com>
2023-06-18 14:53:45 +0100

    Login.js file modified

--- 0x02. React props/task_2/dashboard/src/Login/Login.js
+++ 0x02. React props/task_2/dashboard/src/Login/Login.js
@@ -1,21 +1,21 @@
-import React from "react";
-import "./Login.css";
+import React from 'react'
+import './Login.css'
 
 function Login() {
   return (
     <React.Fragment>
-      <div className="App-body">
+      <div className='App-body'>
         <p>Login to access the full dashboard</p>
         <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password"></input>
+          <label htmlFor='email'>Email:</label>
+          <input type='email' name='email'></input>
+          <label htmlFor='password'>Password:</label>
+          <input type='password' name='password'></input>
           <button>OK</button>
         </form>
       </div>
     </React.Fragment>
-  );
+  )
 }
 
-export default Login;
+export default Login
