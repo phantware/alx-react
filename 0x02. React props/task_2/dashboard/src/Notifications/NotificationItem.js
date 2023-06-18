ab9d357403e090b5e570baf758054c2f237e6ea7
phantware <phantmoney2011@gmail.com>
2023-06-18 14:57:29 +0100

    NotificationItem.js file modified

--- 0x02. React props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x02. React props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,13 +1,15 @@
-import React from "react";
-import "./Notifications.css";
+import React from 'react'
+import './Notifications.css'
 
 function NotificationItem({ type, html, value }) {
   return (
     <>
       {type && value ? <li data-notification-type={type}>{value}</li> : null}
-      {html ? <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li> : null}
+      {html ? (
+        <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
+      ) : null}
     </>
-  );
+  )
 }
 
-export default NotificationItem;
+export default NotificationItem
