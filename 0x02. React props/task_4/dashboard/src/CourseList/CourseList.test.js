760753e1a5809be4ba18c038ece5ffe291542472
phantware <phantmoney2011@gmail.com>
2023-06-19 20:28:57 +0100

    CourseList.test.js file modified

--- 0x02. React props/task_4/dashboard/src/CourseList/CourseList.test.js
+++ 0x02. React props/task_4/dashboard/src/CourseList/CourseList.test.js
@@ -6,13 +6,11 @@ import { shallow } from 'enzyme'
 describe('CourseList component tests', () => {
   it('should render without crashing', () => {
     const wrapper = shallow(<CourseList />)
-
     expect(wrapper.exists()).toBe(true)
   })
 
   it('renders 5 different rows', () => {
     const wrapper = shallow(<CourseList />)
-
     expect(wrapper.find('thead').children()).toHaveLength(2)
     wrapper.find('thead').forEach((node) => {
       expect(node.equals(<CourseListRow textFirstCell='Foo' />))
