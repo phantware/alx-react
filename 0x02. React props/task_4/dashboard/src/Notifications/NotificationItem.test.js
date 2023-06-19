0da486edcba3af47bd78e2f1cef1259c580492a8
phantware <phantmoney2011@gmail.com>
2023-06-19 20:45:35 +0100

    NotificationItem.test.js file modified

--- 0x02. React props/task_4/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x02. React props/task_4/dashboard/src/Notifications/NotificationItem.test.js
@@ -5,13 +5,11 @@ import { shallow } from 'enzyme'
 describe('rendering components', () => {
   it('renders NotificationItem component without crashing', () => {
     const wrapper = shallow(<NotificationItem />)
-
     expect(wrapper.exists()).toBe(true)
   })
 
   it('renders correct html from type="default" value="test" props', () => {
     const wrapper = shallow(<NotificationItem />)
-
     wrapper.setProps({ type: 'default', value: 'test' })
     expect(wrapper.html()).toEqual(
       '<li data-notification-type="default">test</li>'
@@ -20,7 +18,6 @@ describe('rendering components', () => {
 
   it('renders correct html from  html="<u>test</u>" props', () => {
     const wrapper = shallow(<NotificationItem />)
-
     wrapper.setProps({ html: '<u>test</u>' })
     expect(wrapper.html()).toEqual('<li data-urgent="true"><u>test</u></li>')
   })
