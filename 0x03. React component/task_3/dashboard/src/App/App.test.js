c13d81324f6575fbbc95b4fbd334bf12809a7d34
phantware <phantmoney2011@gmail.com>
2023-06-23 20:23:50 +0100

    App.test.js file modified

--- 0x03. React component/task_3/dashboard/src/App/App.test.js
+++ 0x03. React component/task_3/dashboard/src/App/App.test.js
@@ -13,39 +13,37 @@ import { shallow, mount } from 'enzyme'
 describe('App tests', () => {
   it('renders without crashing', () => {
     const component = shallow(<App />)
-
     expect(component).toBeDefined()
   })
+
   it('should render Notifications component', () => {
     const component = shallow(<App />)
-
     expect(component.containsMatchingElement(<Notifications />)).toEqual(false)
   })
+
   it('should render Header component', () => {
     const component = shallow(<App />)
-
     expect(component.contains(<Header />)).toBe(true)
   })
+
   it('should render Login Component', () => {
     const component = shallow(<App />)
-
     expect(component.contains(<Login />)).toBe(true)
   })
+
   it('should render Footer Component', () => {
     const component = shallow(<App />)
-
     expect(component.contains(<Footer />)).toBe(true)
   })
+
   it('does not render courselist if logged out', () => {
     const component = shallow(<App />)
-
     component.setProps({ isLogedIn: false })
-
     expect(component.contains(<CourseList />)).toBe(false)
   })
+
   it('renders courselist if logged in', () => {
     const component = shallow(<App isLoggedIn={true} />)
-
     expect(component.containsMatchingElement(<CourseList />)).toEqual(false)
     expect(component.contains(<Login />)).toBe(false)
   })
@@ -68,7 +66,6 @@ describe('When ctrl + h is pressed', () => {
     const spy = jest.spyOn(window, 'alert')
     const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' })
     document.dispatchEvent(event)
-
     expect(spy).toHaveBeenCalled()
     spy.mockRestore()
     wrapper.unmount()
@@ -79,7 +76,6 @@ describe('When ctrl + h is pressed', () => {
     const spy = jest.spyOn(window, 'alert')
     const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' })
     document.dispatchEvent(event)
-
     expect(spy).toHaveBeenCalledWith('Logging you out')
     jest.restoreAllMocks()
     wrapper.unmount()
