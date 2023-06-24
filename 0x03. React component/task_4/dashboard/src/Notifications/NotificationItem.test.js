345ba0cde582476cd572b57d8d23660d0f16440f
phantware <phantmoney2011@gmail.com>
2023-06-24 11:52:08 +0100

    NotificationItem.test.js file modified

--- 0x03. React component/task_4/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x03. React component/task_4/dashboard/src/Notifications/NotificationItem.test.js
@@ -5,13 +5,11 @@ import { shallow } from 'enzyme'
 describe('rendering components', () => {
   it('renders NotificationItem component without crashing', () => {
     const wrapper = shallow(<NotificationItem />)
-
     expect(wrapper.exists()).toBe(true)
   })
 
   it('renders correct html from type="default" value="test" props', () => {
     const wrapper = shallow(<NotificationItem />)
-
     wrapper.setProps({ type: 'default', value: 'test' })
     expect(wrapper.html()).toEqual(
       '<li data-notification-type="default">test</li>'
@@ -20,7 +18,6 @@ describe('rendering components', () => {
 
   it('renders correct html from  html="<u>test</u>" props', () => {
     const wrapper = shallow(<NotificationItem />)
-
     wrapper.setProps({ html: '<u>test</u>' })
     expect(wrapper.html()).toEqual('<li data-urgent="true"><u>test</u></li>')
   })
@@ -30,7 +27,6 @@ describe('onclick event behaves as it should', () => {
   it('should call console.log', () => {
     const wrapper = shallow(<NotificationItem />)
     const spy = jest.fn()
-
     wrapper.setProps({ value: 'test item', markAsRead: spy, id: 1 })
     wrapper.find('li').props().onClick()
     expect(spy).toBeCalledTimes(1)
