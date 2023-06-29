d691ff934835a5b4f1e2bf46128caf28b172cee6
phantware <phantmoney2011@gmail.com>
2023-06-29 22:50:56 +0100

    App.test.js file modified

--- 0x04. React inline styling/task_3/dashboard/src/App/App.test.js
+++ 0x04. React inline styling/task_3/dashboard/src/App/App.test.js
@@ -21,39 +21,37 @@ afterEach(() => {
 describe('App tests', () => {
   it('renders without crashing', () => {
     const component = shallow(<App />)
-
     expect(component).toBeDefined()
   })
+
   it('should render Notifications component', () => {
     const component = shallow(<App />)
-
     expect(component.containsMatchingElement(<Notifications />)).toEqual(false)
   })
+
   it('should render Header component', () => {
     const component = shallow(<App />)
-
     expect(component.contains(<Header />)).toBe(true)
   })
+
   it('should render Login Component', () => {
     const component = shallow(<App />)
-
     expect(component.contains(<Login />)).toBe(true)
   })
+
   it('should render Footer Component', () => {
     const component = shallow(<App />)
-
     expect(component.contains(<Footer />)).toBe(true)
   })
+
   it('does not render courselist if logged out', () => {
     const component = shallow(<App />)
-
     component.setProps({ isLogedIn: false })
-
     expect(component.contains(<CourseList />)).toBe(false)
   })
+
   it('renders courselist if logged in', () => {
     const component = shallow(<App isLoggedIn={true} />)
-
     expect(component.containsMatchingElement(<CourseList />)).toEqual(false)
     expect(component.contains(<Login />)).toBe(false)
   })
