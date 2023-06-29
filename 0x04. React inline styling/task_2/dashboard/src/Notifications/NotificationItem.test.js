1544b4950c06c920ad0c79c203d0b0960fa04553
phantware <phantmoney2011@gmail.com>
2023-06-29 22:17:01 +0100

    Notification.test.js file modified

--- 0x04. React inline styling/task_2/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x04. React inline styling/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -37,6 +37,7 @@ describe('onclick event behaves as it should', () => {
   it('should call console.log', () => {
     const wrapper = shallow(<NotificationItem />)
     const spy = jest.fn()
+
     wrapper.setProps({ value: 'test item', markAsRead: spy, id: 1 })
     wrapper.find('li').props().onClick()
     expect(spy).toBeCalledTimes(1)
