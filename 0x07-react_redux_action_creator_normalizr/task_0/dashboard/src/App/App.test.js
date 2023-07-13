8e92503404f00d7f925d95a775e263858dd58076
phantware <phantmoney2011@gmail.com>
2023-07-13 20:58:08 +0100

    App.test.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/App/App.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/App/App.test.js
@@ -22,37 +22,31 @@ afterEach(() => {
 describe('rendering components', () => {
   it('renders App component without crashing', () => {
     const wrapper = shallow(<App />)
-
     expect(wrapper.exists()).toBe(true)
   })
 
   it('contains Notifications component', () => {
     const wrapper = shallow(<App />)
-
     expect(wrapper.find(Notifications)).toHaveLength(1)
   })
 
   it('contains Header component', () => {
     const wrapper = shallow(<App />)
-
     expect(wrapper.contains(<Header />)).toBe(true)
   })
 
   it('contains Login component', () => {
     const wrapper = shallow(<App />)
-
     expect(wrapper.find(Login)).toHaveLength(1)
   })
 
   it('contains Footer component', () => {
     const wrapper = shallow(<App />)
-
     expect(wrapper.contains(<Footer />)).toBe(true)
   })
 
   it('checks CourseList is not rendered', () => {
     const wrapper = shallow(<App />)
-
     expect(wrapper.contains(<CourseList />)).toBe(false)
   })
 })
@@ -60,7 +54,6 @@ describe('rendering components', () => {
 describe('when isLogged in is true', () => {
   const wrapper = shallow(<App />)
   expect(wrapper.state().user).toEqual(user)
-
   it('checks Login is not rendered', () => {
     expect(wrapper.contains(<Login />)).toBe(false)
   })
@@ -113,21 +106,18 @@ describe('when isLogged in is true', () => {
 describe('testing state of App.js', () => {
   it('displayDrawer initial value should be set to false', () => {
     const wrapper = mount(<App />)
-
     expect(wrapper.state().displayDrawer).toBe(false)
   })
 
   it('should set displayDrawer to true after calling handleDisplayDrawer', () => {
     const wrapper = shallow(<App />)
     wrapper.instance().handleDisplayDrawer()
-
     expect(wrapper.state().displayDrawer).toBe(true)
   })
 
   it('should set displayDrawer to false after calling handleHideDrawer', () => {
     const wrapper = shallow(<App />)
     wrapper.instance().handleHideDrawer()
-
     expect(wrapper.state().displayDrawer).toBe(false)
   })
 })
@@ -163,7 +153,6 @@ describe('markNotificationAsRead works as intended', () => {
 
     expect(wrapper.state().listNotifications.length).toBe(2)
     expect(wrapper.state().listNotifications[3]).toBe(undefined)
-
     wrapper.unmount()
   })
 })
