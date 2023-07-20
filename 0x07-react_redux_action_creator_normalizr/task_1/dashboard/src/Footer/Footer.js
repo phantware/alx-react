2a39582d00d618ed75419262a942e09421b4a59c
phantware <phantmoney2011@gmail.com>
2023-07-20 19:25:35 +0100

    Footer.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/Footer/Footer.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/Footer/Footer.js
@@ -1,17 +1,17 @@
-import React, { useContext } from "react";
-import "./Footer.css";
-import { getFullYear, getFooterCopy } from "../utils/utils";
-import { AppContext } from "../App/AppContext";
+import React, { useContext } from 'react'
+import './Footer.css'
+import { getFullYear, getFooterCopy } from '../utils/utils'
+import { AppContext } from '../App/AppContext'
 
 function Footer() {
-  const { user } = useContext(AppContext);
+  const { user } = useContext(AppContext)
 
   return (
     <>
-      <div className="App-footer">
+      <div className='App-footer'>
         {user.isLoggedIn && (
           <p>
-            <a href="#">Contact us</a>
+            <a href='#'>Contact us</a>
           </p>
         )}
         <p>
@@ -19,7 +19,7 @@ function Footer() {
         </p>
       </div>
     </>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
