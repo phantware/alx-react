86bbcfc3412438daf5f0fb039cf8015c85e26f42
phantware <phantmoney2011@gmail.com>
2023-07-28 15:55:18 +0100

    courseActionCreators.test.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
@@ -5,6 +5,7 @@ describe('tests for action creators', () => {
   it('should return right action payload and type when selectCourse is called', () => {
     expect(selectCourse(1)).toEqual({ type: SELECT_COURSE, index: 1 })
   })
+
   it('should return right action payload and type when unSelectCourse is called', () => {
     expect(unSelectCourse(1)).toEqual({ type: UNSELECT_COURSE, index: 1 })
   })
