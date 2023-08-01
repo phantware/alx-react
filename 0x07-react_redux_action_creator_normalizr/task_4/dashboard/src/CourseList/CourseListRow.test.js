8c2705f45b7b10c3f6b54d3c3b2cd9813e12c035
phantware <phantmoney2011@gmail.com>
2023-08-01 16:22:53 +0100

    CourseListRow.test.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/CourseList/CourseListRow.test.js
@@ -13,7 +13,6 @@ afterEach(() => {
 describe('Course List Row component test', () => {
   it('should render without crashing', () => {
     const wrapper = shallow(<CourseListRow textFirstCell='test' />)
-
     expect(wrapper.exists()).toBe(true)
   })
 
