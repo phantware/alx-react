cacd67ccca20528e367c3253f902cda0a83eddab
phantware <phantmoney2011@gmail.com>
2023-08-03 13:51:23 +0100

    NotificationItem.test.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/Notifications/NotificationItem.test.js
@@ -13,13 +13,11 @@ afterEach(() => {
 describe('rendering components', () => {
   it('renders NotificationItem component without crashing', () => {
     const wrapper = shallow(<NotificationItem />)
-
     expect(wrapper.exists()).toBe(true)
   })
 
   it('renders correct html from type="default" value="test" props', () => {
     const wrapper = shallow(<NotificationItem />)
-
     wrapper.setProps({ type: 'default', value: 'test' })
     expect(wrapper.html()).toEqual(
       '<li class="default_2c02es" data-notification-type="default">test</li>'
@@ -28,7 +26,6 @@ describe('rendering components', () => {
 
   it('renders correct html from  html="<u>test</u>" props', () => {
     const wrapper = shallow(<NotificationItem />)
-
     wrapper.setProps({ html: '<u>test</u>' })
     expect(wrapper.html()).toEqual(
       '<li data-urgent="true" class="urgent_cyonix"><u>test</u></li>'
