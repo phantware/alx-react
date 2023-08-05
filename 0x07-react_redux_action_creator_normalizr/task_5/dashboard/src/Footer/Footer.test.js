756070dfdc75ecccef947e8f2c614429833fa36d
phantware <phantmoney2011@gmail.com>
2023-08-05 13:12:41 +0100

    Footer.test.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/Footer/Footer.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/Footer/Footer.test.js
@@ -15,7 +15,6 @@ describe('Footer test', () => {
 
   it('Footer component renders "Copyright ${getFullYear()} - ${getFooterCopy(true)}"', () => {
     const wrapper = mount(<Footer />)
-
     expect(wrapper.find('p').text()).toEqual(
       `Copyright ${getFullYear()} - ${getFooterCopy(false)}`
     )
@@ -39,7 +38,6 @@ describe('Footer test', () => {
     expect(wrapper.find('a').length).toBe(0)
     expect(wrapper.find('a').exists()).toBe(false)
     expect(wrapper.text()).not.toContain('Contact us')
-
     wrapper.unmount()
   })
 
@@ -61,7 +59,6 @@ describe('Footer test', () => {
     expect(wrapper.find('a').length).toBe(1)
     expect(wrapper.find('a').exists()).toBe(true)
     expect(wrapper.text()).toContain('Contact us')
-
     wrapper.unmount()
   })
 })
