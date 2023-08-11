ed77d04bd2680de89813d87f42795f516005bbb8
phantware <phantmoney2011@gmail.com>
2023-08-11 19:13:58 +0100

    notificationActionCreators.test.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.test.js
@@ -10,7 +10,6 @@ describe('tests for notification actions', () => {
   it('should create right action for mark as read', () => {
     expect(markAsRead(1)).toEqual({ type: MARK_AS_READ, index: 1 })
   })
-
   it('should create right action for notification filter', () => {
     expect(setNotificationFilter(NotificationTypeFilters['DEFAULT'])).toEqual({
       type: SET_TYPE_FILTER,
