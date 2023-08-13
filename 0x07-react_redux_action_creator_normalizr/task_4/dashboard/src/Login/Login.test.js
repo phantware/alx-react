f12871601cd81be7d9466c6b38394de18ed9398e
phantware <phantmoney2011@gmail.com>
2023-08-13 18:30:51 +0100

    Login.test.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/Login/Login.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/Login/Login.test.js
@@ -25,13 +25,11 @@ describe('Header', () => {
 describe('test for submit input on form', () => {
   it('verify that the submit button is disabled by default', () => {
     const wrapper = shallow(<Login />)
-
     expect(wrapper.find("input[type='submit']").props().disabled).toEqual(true)
   })
 
   it('verify that after changing the value of the two inputs, the button is enabled', () => {
     const wrapper = shallow(<Login />)
-
     wrapper.find('#email').simulate('change', { target: { value: 't' } })
     wrapper.find('#password').simulate('change', { target: { value: 't' } })
     expect(wrapper.find("input[type='submit']").props().disabled).toEqual(true)
