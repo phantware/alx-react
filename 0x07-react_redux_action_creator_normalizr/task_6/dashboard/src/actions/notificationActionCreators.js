c0fd1518396d3c523cf87bdaf8f8025bc07f943a
phantware <phantmoney2011@gmail.com>
2023-09-03 16:47:55 +0100

    notificationActionCreators.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.js
@@ -1,19 +1,24 @@
-import { NotificationTypeFilters, MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
+import {
+  NotificationTypeFilters,
+  MARK_AS_READ,
+  SET_TYPE_FILTER
+} from './notificationActionTypes'
 
 export const markAsRead = (index) => {
   return {
     type: MARK_AS_READ,
-    index,
-  };
-};
+    index
+  }
+}
 
-export const boundMarkAsRead = (index) => dispatch(markAsRead(index));
+export const boundMarkAsRead = (index) => dispatch(markAsRead(index))
 
 export const setNotificationFilter = (filter) => {
   return {
     type: SET_TYPE_FILTER,
-    filter,
-  };
-};
+    filter
+  }
+}
 
-export const boundSetNotificationFilter = (filter) => dispatch(setNotificationFilter(filter));
+export const boundSetNotificationFilter = (filter) =>
+  dispatch(setNotificationFilter(filter))
