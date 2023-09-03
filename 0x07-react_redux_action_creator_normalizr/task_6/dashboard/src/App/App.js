3add0d6e2c2889eff42e3932339de76f00f6c34d
phantware <phantmoney2011@gmail.com>
2023-09-03 16:51:29 +0100

    App.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/App/App.js
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/App/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Login from "../Login/Login";
-import CourseList from "../CourseList/CourseList";
-import Notifications from "../Notifications/Notifications";
-import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
-import BodySection from "../BodySection/BodySection";
-import { StyleSheet, css } from "aphrodite";
-import PropTypes from "prop-types";
-import { getLatestNotification } from "../utils/utils";
-import { AppContext, user } from "./AppContext";
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import Login from '../Login/Login'
+import CourseList from '../CourseList/CourseList'
+import Notifications from '../Notifications/Notifications'
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom'
+import BodySection from '../BodySection/BodySection'
+import { StyleSheet, css } from 'aphrodite'
+import PropTypes from 'prop-types'
+import { getLatestNotification } from '../utils/utils'
+import { AppContext, user } from './AppContext'
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       displayDrawer: false,
@@ -21,48 +21,48 @@ class App extends React.Component {
       logOut: this.logOut,
 
       listNotifications: [
-        { id: 1, type: "default", value: "New course available" },
-        { id: 2, type: "urgent", value: "New resume available" },
-        { id: 3, type: "urgent", html: getLatestNotification() },
-      ],
-    };
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: getLatestNotification() }
+      ]
+    }
 
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
-    this.handleHideDrawer = this.handleHideDrawer.bind(this);
-    this.logIn = this.logIn.bind(this);
-    this.logOut = this.logOut.bind(this);
-    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this)
+    this.handleHideDrawer = this.handleHideDrawer.bind(this)
+    this.logIn = this.logIn.bind(this)
+    this.logOut = this.logOut.bind(this)
+    this.markNotificationAsRead = this.markNotificationAsRead.bind(this)
   }
 
   listCourses = [
-    { id: 1, name: "ES6", credit: 60 },
-    { id: 2, name: "Webpack", credit: 20 },
-    { id: 3, name: "React", credit: 40 },
-  ];
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 }
+  ]
 
   handleKeyPress(e) {
-    if (e.ctrlKey && e.key === "h") {
-      e.preventDefault();
-      alert("Logging you out");
-      this.props.logOut();
+    if (e.ctrlKey && e.key === 'h') {
+      e.preventDefault()
+      alert('Logging you out')
+      this.props.logOut()
     }
   }
 
   handleDisplayDrawer() {
-    this.setState({ displayDrawer: true });
+    this.setState({ displayDrawer: true })
   }
 
   handleHideDrawer() {
-    this.setState({ displayDrawer: false });
+    this.setState({ displayDrawer: false })
   }
 
   componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyPress);
+    document.addEventListener('keydown', this.handleKeyPress)
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyPress);
+    document.removeEventListener('keydown', this.handleKeyPress)
   }
 
   logIn(email, password) {
@@ -70,32 +70,34 @@ class App extends React.Component {
       user: {
         email,
         password,
-        isLoggedIn: true,
-      },
-    });
+        isLoggedIn: true
+      }
+    })
   }
 
   logOut() {
     this.setState({
-      user: user,
-    });
+      user: user
+    })
   }
 
   markNotificationAsRead(id) {
-    const newList = this.state.listNotifications.filter((notification) => notification.id !== id);
-    this.setState({ listNotifications: newList });
+    const newList = this.state.listNotifications.filter(
+      (notification) => notification.id !== id
+    )
+    this.setState({ listNotifications: newList })
   }
   render() {
     return (
       <AppContext.Provider
         value={{
           user: this.state.user,
-          logout: this.state.logOut,
+          logout: this.state.logOut
         }}
       >
         <React.Fragment>
           <div className={css(styles.App)}>
-            <div className="heading-section">
+            <div className='heading-section'>
               <Notifications
                 markNotificationAsRead={this.markNotificationAsRead}
                 listNotifications={this.state.listNotifications}
@@ -106,47 +108,49 @@ class App extends React.Component {
               <Header />
             </div>
             {this.state.user.isLoggedIn ? (
-              <BodySectionWithMarginBottom title="Course list">
+              <BodySectionWithMarginBottom title='Course list'>
                 <CourseList listCourses={this.listCourses} />
               </BodySectionWithMarginBottom>
             ) : (
-              <BodySectionWithMarginBottom title="Log in to continue">
+              <BodySectionWithMarginBottom title='Log in to continue'>
                 <Login logIn={this.logIn} />
               </BodySectionWithMarginBottom>
             )}
-            <BodySection title="News from the school">
+            <BodySection title='News from the school'>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis at tempora odio, necessitatibus repudiandae reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo
-                ipsa iste vero dolor voluptates.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perspiciatis at tempora odio, necessitatibus repudiandae
+                reiciendis cum nemo sed asperiores ut molestiae eaque aliquam
+                illo ipsa iste vero dolor voluptates.
               </p>
             </BodySection>
             <Footer />
           </div>
         </React.Fragment>
       </AppContext.Provider>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   App: {
-    height: "100vh",
-    maxWidth: "100vw",
-    position: "relative",
-    fontFamily: "Arial, Helvetica, sans-serif",
-  },
-});
+    height: '100vh',
+    maxWidth: '100vw',
+    position: 'relative',
+    fontFamily: 'Arial, Helvetica, sans-serif'
+  }
+})
 
 App.defaultProps = {
   isLoggedIn: false,
   logOut: () => {
-    return;
-  },
-};
+    return
+  }
+}
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
-};
+  logOut: PropTypes.func
+}
 
-export default App;
+export default App
