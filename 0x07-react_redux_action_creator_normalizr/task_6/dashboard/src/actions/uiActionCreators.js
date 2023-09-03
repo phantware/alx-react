0fe7809298824d9fe209e7774e68a854d4966d4b
phantware <phantmoney2011@gmail.com>
2023-09-03 16:49:46 +0100

    uiActionCreators.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -1,26 +1,33 @@
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER
+} from './uiActionTypes'
 
 export function login(email, password) {
   return {
     type: LOGIN,
-    user: { email, password },
-  };
+    user: { email, password }
+  }
 }
 
-export const boundLogin = (email, password) => dispatch(login(email, password));
+export const boundLogin = (email, password) => dispatch(login(email, password))
 
-export const logout = () => ({ type: LOGOUT });
+export const logout = () => ({ type: LOGOUT })
 
-export const boundLogout = () => dispatch(logout());
+export const boundLogout = () => dispatch(logout())
 
 export const displayNotificationDrawer = () => ({
-  type: DISPLAY_NOTIFICATION_DRAWER,
-});
+  type: DISPLAY_NOTIFICATION_DRAWER
+})
 
-export const boundDisplayNotificationDrawer = () => dispatch(displayNotificationDrawer());
+export const boundDisplayNotificationDrawer = () =>
+  dispatch(displayNotificationDrawer())
 
 export const hideNotificationDrawer = () => ({
-  type: HIDE_NOTIFICATION_DRAWER,
-});
+  type: HIDE_NOTIFICATION_DRAWER
+})
 
-export const boundHideNotificationDrawer = () => dispatch(hideNotificationDrawer());
+export const boundHideNotificationDrawer = () =>
+  dispatch(hideNotificationDrawer())
