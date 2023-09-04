70ffdd77d583469ddb1d4ece795be5b0ffc7751a
phantware <phantmoney2011@gmail.com>
2023-09-04 16:43:17 +0100

    courseActionCreator.js file modified

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/courseActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/courseActionCreators.js
@@ -1,19 +1,19 @@
-import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes'
 
 export function selectCourse(index) {
   return {
     type: SELECT_COURSE,
-    index: index,
-  };
+    index: index
+  }
 }
 
-export const boundSelectCourse = (index) => dispatch(selectCourse(index));
+export const boundSelectCourse = (index) => dispatch(selectCourse(index))
 
 export function unSelectCourse(index) {
   return {
     type: UNSELECT_COURSE,
-    index: index,
-  };
+    index: index
+  }
 }
 
-export const boundUnSelectCourse = (index) => dispatch(unSelectCourse(index));
+export const boundUnSelectCourse = (index) => dispatch(unSelectCourse(index))
